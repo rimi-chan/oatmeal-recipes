f252e7cdfe9b93816293f32f4cc753116a417a71
EC2 Default User <ec2-user@ip-172-31-24-198.ap-northeast-1.compute.internal>
2022-11-29 14:56:59 +0000

    モデル同士の関連付け

--- app/models/oat.rb
+++ app/models/oat.rb
@@ -1,3 +1,4 @@
 class Oat < ApplicationRecord
+  belongs_to :user, optional: true
   mount_uploader :oat_image_id, OatImageIdUploader
 end
--- app/models/powder.rb
+++ app/models/powder.rb
@@ -1,3 +1,4 @@
 class Powder < ApplicationRecord
+  belongs_to :user, optional: true
   mount_uploader :powder_image_id, PowderImageIdUploader
 end
