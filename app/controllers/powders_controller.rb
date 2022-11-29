e6d29c1c6eb450507996d17768b36e753150965d
EC2 Default User <ec2-user@ip-172-31-24-198.ap-northeast-1.compute.internal>
2022-11-29 15:35:51 +0000

    powderモデルの投稿/詳細/一覧画面の実装

--- app/controllers/powders_controller.rb
+++ app/controllers/powders_controller.rb
@@ -1,19 +1,32 @@
 class PowdersController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:index,:show]
+  
   def index
+    @powders = Powder.all
   end
 
   def new
-  end
-
-  def show
-  end
-
-  def search
+    @powder = Powder.new
   end
 
   def create
+    @powder = current_user.powders.build(powder_params)
+    if @powder.save
+      redirect_to powder_path(@powder)
+    else 
+      render :new
+    end
   end
-
-  def edit
+  
+  def show
+    @powder = Powder.find_by(id:params[:id])
   end
+  
+  
+  private
+  def powder_params
+    params.require(:powder).permit(:powder_name, :powder_image_id, :powder_tool, :powder_word, :powder_instructions)
+  end
+  
 end
