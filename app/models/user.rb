2e09a5ed6b1807d1bdd5892fdc0a391762ddc1ff
EC2 Default User <ec2-user@ip-172-31-24-198.ap-northeast-1.compute.internal>
2022-11-29 16:25:17 +0000

    useモデルの画像投稿機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          
   has_many :oats, dependent: :destroy
   has_many :powders, dependent: :destroy
+  
+    
+  mount_uploader :user_image_id, UserImageIdUploader
+  mount_uploader :profile_image_id, ProfileImageIdUploader
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_081603) do
+ActiveRecord::Schema.define(version: 2022_11_29_161541) do
 
   create_table "oats", charset: "utf8mb4", force: :cascade do |t|
     t.string "oat_name"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2022_11_29_081603) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "user_image_id"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
