9aa50ece092ee890c1ea63a581dabab204f31cf6
EC2 Default User <ec2-user@ip-172-31-24-198.ap-northeast-1.compute.internal>
2022-11-29 15:16:20 +0000

    oatモデルの投稿/詳細/一覧画面の実装

--- app/controllers/oats_controller.rb
+++ app/controllers/oats_controller.rb
@@ -1,19 +1,32 @@
 class OatsController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:index,:show]
+  
   def index
+    @oats = Oat.all
   end
 
   def new
-  end
-
-  def show
-  end
-
-  def search
+    @oat = Oat.new
   end
 
   def create
+    @oat = current_user.oats.build(oat_params)
+    if @oat.save
+      redirect_to oat_path(@oat)
+    else 
+      render :new
+    end
   end
-
-  def edit
+  
+  def show
+    @oat = Oat.find_by(id:params[:id])
   end
+  
+  
+  private
+  def oat_params
+    params.require(:oat).permit(:oat_name, :oat_image_id, :oat_tool, :oat_word, :oat_instructions)
+  end
+  
 end
