65f1ece9e6c2f9848d4ca2160c5c77b8204021fa
EC2 Default User <ec2-user@ip-172-31-24-198.ap-northeast-1.compute.internal>
2022-12-08 10:41:16 +0000

    bootstrapの変更内容を元に戻す

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
-import 'bootstrap'
-import '../stylesheets/application'
+import 'bootstrap';
+import '../stylesheets/application.scss';
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
